23dbd7ec9460eca7ffe99acf5ee2ee317604c0ba
Priya Power <priyajag72@gmail.com>
2020-08-02 15:03:17 -0600

    Add setup and test_it_can_read_and_get_details_from_input_txt_file with stub for command line argument and assertions for read and character count

--- test/file_reader_writer_test.rb
+++ test/file_reader_writer_test.rb
@@ -2,12 +2,18 @@ require_relative "test_helper"
 
 class FileReaderWriterTest < Minitest::Test
 
-  def test_it_exists
-    file = FileReaderWriter.new
-    assert_instance_of FileReaderWriter, file
+  def setup
+    @file = FileReaderWriter.new
   end
 
-  #def test_it_has_attributes
-  #end
+  def test_it_exists
+    assert_instance_of FileReaderWriter, @file
+  end
 
+  def test_it_can_read_and_get_details_from_input_txt_file
+    @file.stubs(:input).returns("message.txt")
+
+    assert_equal "Hello Turing!\n", @file.reader
+    assert_equal 13, @file.input_character_count
+  end
 end
