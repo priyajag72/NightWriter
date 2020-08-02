12359531da3283e3e01a0b5628246a2b6a32c920
Priya Power <priyajag72@gmail.com>
2020-08-02 15:49:46 -0600

    Add setup with class object for quick testing and test_it_can_print_to_terminal_io_confirmation_message with stubbes for command line arguments

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -2,12 +2,19 @@ require_relative "test_helper"
 
 class NightWriterTest < Minitest::Test
 
-  def test_it_exists
-    night_writer = NightWriter.new
-    assert_instance_of NightWriter, night_writer
+  def setup
+    @night_writer = NightWriter.new
   end
 
-  #def test_it_has_attributes
-  #end
+  def test_it_exists
+    assert_instance_of NightWriter, @night_writer
+  end
+
+  def test_it_can_print_to_terminal_io_confirmation_message
+    @night_writer.stubs(:input).returns("message.txt")
+    @night_writer.stubs(:output).returns("braille.txt")
+
+    assert_equal "Created 'braille.txt' containing 14 characters", @night_writer.confirmation_message
+  end
 
 end
