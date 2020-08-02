0bac5ae218a1e30963f9c9f47ff97e4d8c4e844c
Priya Power <priyajag72@gmail.com>
2020-08-02 16:55:44 -0600

    Update test_it_has_attributes with connections to Class FileReaderWriter as I transition to command line argument testing

--- test/night_writer_test.rb
+++ test/night_writer_test.rb
@@ -10,9 +10,19 @@ class NightWriterTest < Minitest::Test
     assert_instance_of NightWriter, @night_writer
   end
 
+  def test_it_has_attributes
+    @night_writer.reader.stubs(:input).returns("message.txt")
+    @night_writer.reader.stubs(:output).returns("braille.txt")
+
+    assert_equal "Hello Turing!\n", @night_writer.file_input
+    assert_equal 14, @night_writer.character_count(:input)
+    assert_equal "Hello Turing!\n", @night_writer.file_output
+    assert_equal 14, @night_writer.character_count(:output)
+  end
+
   def test_it_can_print_to_terminal_io_confirmation_message
-    @night_writer.stubs(:input).returns("message.txt")
-    @night_writer.stubs(:output).returns("braille.txt")
+    @night_writer.reader.stubs(:input).returns("message.txt")
+    @night_writer.reader.stubs(:output).returns("braille.txt")
 
     assert_equal "Created 'braille.txt' containing 14 characters", @night_writer.confirmation_message
   end
