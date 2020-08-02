99264217d8e1f2769cdaabd9156d94cf15c70e95
Priya Power <priyajag72@gmail.com>
2020-08-02 16:55:14 -0600

    Add confirmation testing of Class FileReaderWriter with confirmation message updated

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -1,9 +1,35 @@
 require_relative "file_reader_writer"
 
-class NightWriter < FileReaderWriter
+class NightWriter
+
+  attr_reader :reader
+
+  def initialize
+    @reader = FileReaderWriter.new
+  end
+
+  def file_input
+    @reader.reader
+  end
+
+  def file_output
+    @reader.writer
+  end
+
+  def character_count(io)
+    case
+      when :input
+        @reader.input_character_count
+      when :output
+        @reader.output_character_count
+    end
+  end
 
   def confirmation_message
-    require "pry"; binding.pry
+    "Created '#{@reader.output}' containing #{character_count(:output)} characters"
   end
 
 end
+
+# x = NightWriter.new
+# x.confirmation_message
