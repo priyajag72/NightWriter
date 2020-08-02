bcae60018503cb03f047bfdecc5e67c2ee6bb33d
Priya Power <priyajag72@gmail.com>
2020-08-02 15:03:43 -0600

    Add method reader which accesses command line argument and reads input file which already exists

--- lib/file_reader_writer.rb
+++ lib/file_reader_writer.rb
@@ -1,2 +1,13 @@
 class FileReaderWriter
+
+  attr_reader :input, :output
+
+  def initialize
+    @input = ARGV[0]
+    @output = ARGV[1]
+  end
+
+  def reader
+    File.open(input) { |file| file.read() }
+  end
 end
