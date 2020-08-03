a0c0bc53cf8dace23574dc99a0da309a905af25f
Priya Power <priyajag72@gmail.com>
2020-08-03 03:46:51 -0600

    Update @dictionary argument to hold lowercase csv file_location

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -1,11 +1,12 @@
 # Capital letters just have a dot6 before the character or a double dot6 before the word
+# "./dictionary/lowercase_to_international_braille.csv"
 
 require_relative "test_helper"
 
 class DictionaryTest < Minitest::Test
-dictionary/lowercase_to_international_braille.csv
   def setup
-    @dictionary = Dictionary.new()
+    lowercase = "./dictionary/lowercase_to_international_braille.csv"
+    @dictionary = Dictionary.new(lowercase)
 
   end
 
