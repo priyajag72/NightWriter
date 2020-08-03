ae1100b9009b3323af615e96947942868705aa52
Priya Power <priyajag72@gmail.com>
2020-08-03 03:48:03 -0600

    Add class Matrix with custom defined matrix components

--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,6 +1,15 @@
+require 'matrix'
+require 'csv'
+
+class Matrix
+  def []=(i, j, x)
+    @rows[i][j] = x
+  end
+end
+
 class Dictionary
 
-  def initialize(argument)
-    @argument = argument
+  def initialize(file_location)
+    @file_location = file_location
   end
 end
