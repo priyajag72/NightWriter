9f1965df6f676afeac564c119463415f89712cc5
Priya Power <priyajag72@gmail.com>
2020-08-03 14:36:15 -0600

    Update assertions for dummy message lowercase and no punctuation

--- test/file_reader_writer_test.rb
+++ test/file_reader_writer_test.rb
@@ -13,8 +13,8 @@ class FileReaderWriterTest < Minitest::Test
   def test_it_can_read_and_get_details_from_input_txt_file
     @file.stubs(:input).returns("message.txt")
 
-    assert_equal "Hello Turing!\n", @file.reader
-    assert_equal 14, @file.input_character_count
+    assert_equal "hello turing\n", @file.reader
+    assert_equal 13, @file.input_character_count
   end
 
   def test_it_can_write_a_new_file_and_copy_from_input
@@ -22,7 +22,7 @@ class FileReaderWriterTest < Minitest::Test
     @file.stubs(:output).returns("braille.txt")
 
     assert_equal File, @file.creator.class
-    assert_equal "Hello Turing!\n", @file.writer
-    assert_equal 14, @file.output_character_count
+    assert_equal "hello turing\n", @file.writer
+    assert_equal 13, @file.output_character_count
   end
 end
