f378a5a456854977c6a6cd1fa280d024082f9015
Priya Power <priyajag72@gmail.com>
2020-08-04 06:57:54 -0600

    Add reader method to attr and object instance in initialize

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,2 +1,25 @@
+# require_relative "file_reader_writer"
+# require_relative "dictionary"
+
 class NightReader
+
+  attr_reader :reader
+              # :lower_dictionary,
+              # :upper_dictionary,
+              # :punctuation_dictionary,
+              # :numbers_dictionary
+
+  def initialize
+    @reader = FileReaderWriter.new
+
+    # lowercase = "./dictionary/lowercase_to_international_braille.csv"
+    # uppercase = "./dictionary/uppercase_to_international_braille.csv"
+    # punctuation = "./dictionary/punctuation_to_international_braille.csv"
+    # numbers = "./dictionary/numbers_to_international_braille.csv"
+    #
+    # @lower_dictionary = Dictionary.new(lowercase)
+    # @upper_dictionary = Dictionary.new(uppercase)
+    # @punctuation_dictionary = Dictionary.new(punctuation)
+    # @numbers_dictionary = Dictionary.new(numbers)
+  end
 end
