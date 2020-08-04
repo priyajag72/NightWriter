364e2e8d253f9755e2b6e1d7e86613be463d27bc
Priya Power <priyajag72@gmail.com>
2020-08-04 06:57:24 -0600

    Add test it has attributes for confirmation of object file

--- test/night_reader_test.rb
+++ test/night_reader_test.rb
@@ -9,4 +9,15 @@ class NightReaderTest < Minitest::Test
   def test_it_exists
     assert_instance_of NightReader, @night_reader
   end
+
+  def test_it_has_attributes
+    @night_reader.reader.stubs(:input).returns("braille.txt")
+    @night_reader.reader.stubs(:output).returns("message.txt")
+
+    assert_equal "0.0.0.0.0.  .0  0.00  000..00000  000.0.  000.0..000  000.0.0.0.00.00.0..0\n00.00.0..0  0.  ....  00.00..000  0..000  0..00000.0  ..00..00....00.0000.\n....0.0.0.  ..  ..0.  ..0...0...  ..0.0.  ..0.0.0.00  ......0.....0...0.0.\n", @night_reader.reader.reader
+
+    assert_equal 225, @night_reader.reader.character_count(@night_reader.reader.input)
+    # assert_equal 225, @night_reader.reader.input_character_count
+  end
+
 end
