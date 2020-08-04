0ffdb659d3a05b99ae4d1f55cb175a6b8842bef9
Priya Power <priyajag72@gmail.com>
2020-08-03 23:11:29 -0600

    Refactor Dictionary Test

--- test/dictionary_test.rb
+++ test/dictionary_test.rb
@@ -4,9 +4,8 @@ require_relative "test_helper"
 
 class DictionaryTest < Minitest::Test
   def setup
-    lowercase = "./dictionary/lowercase_to_international_braille.csv"
-    @dictionary = Dictionary.new(lowercase)
-
+    @lowercase = "./dictionary/lowercase_to_international_braille.csv"
+    @dictionary = Dictionary.new(@lowercase)
   end
 
   def test_it_exists
@@ -14,38 +13,11 @@ class DictionaryTest < Minitest::Test
   end
 
   def test_it_has_attributes
-    assert_equal "./dictionary/lowercase_to_international_braille.csv", @dictionary.file_location
-
-    @dictionary.reader.stubs(:input).returns("message.txt")
-    assert_equal FileReaderWriter, @dictionary.reader.class
-
-    assert_equal 70, @dictionary.reader.input_character_count
+    assert_equal @lowercase, @dictionary.file_location
+    assert_instance_of FileReaderWriter, @dictionary.reader
   end
 
-  def test_it_can_convert_csv_to_braille_dots_integer_hash
-    assert_equal 1, @dictionary.convert_csv_to_hash["a"]
-    assert_equal 1346, @dictionary.convert_csv_to_hash["x"]
+  def test_it_can_use_a_local_dictionary_and_encode
+    assert_equal [["0.0.0.0.0.", "00.00.0..0", "....0.0.0."]], @dictionary.encode("hello")
   end
-
-  def test_it_can_convert_a_braille_board_from_dots_to_zeros
-    a = Matrix[["0","."],[".","."],[".","."]]
-    assert_equal a, @dictionary.convert(1)
-
-    c = Matrix[["0","0"],[".","."],[".","."]]
-    assert_equal c, @dictionary.convert(14)
-
-    q = Matrix[["0","0"],["0","0"],["0","."]]
-    assert_equal q, @dictionary.convert(12345)
-  end
-
-  def test_it_can_convert_braille_matrix_to_string
-    assert_equal "0.....", @dictionary.convert_braille_matrix_to_string["a"]
-    assert_equal "0000..", @dictionary.convert_braille_matrix_to_string["g"]
-  end
-
-  def test_it_can_use_dictionary_with_one_method
-    assert_equal "0000..", @dictionary.convert_text_english_to_braille("g")
-    assert_equal "0.....0000..", @dictionary.convert_text_english_to_braille("ag")
-  end
-
 end
